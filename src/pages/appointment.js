42d0ab1a91e7d33f523272aa8cdef9b9abcb0076
Hamza Asad <ma06762@st.habib.edu.pk>
2022-05-07 03:19:59 +0500

    issues need fixing, iframe added

--- src/pages/appointment.js
+++ src/pages/appointment.js
@@ -1,33 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import { ErrorMessage, Form, Formik, Field } from "formik"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSpinner } from "@fortawesome/free-solid-svg-icons"
 
 export default function Appointment(props) {
+
+    const [isLoading, setIsLoading] = useState(true)
+
     return (
         <main>
             <title>Appointment</title>
 
             <div className="bg-black m-auto flex justify-center m-4 rounded">
                 <div className="m-auto w-2/3 text-center p-4 rounded">
-                    <h1 className="emphasize-text text-white font-serif text-3xl">Book an Appointment</h1>
-                    <Formik initialValues={{
-                        name: ''
-                    }} onSubmit={(values, { setSubmitting }) => { setSubmitting(false) }}>
-                        {({ isSubmitting }) => (
-                            <Form>
-                                <div className="flex flex-col items-start py-2">
-                                    <label className="text-white text-sm" htmlFor="name">Full Name</label>
-                                    <Field type="text" name="name" autocomplete="name" />
+                    <h1 className="emphasize-text text-white font-serif text-3xl my-4">Book an Appointment</h1>
+                    {
+                        isLoading ?
+                            <div className="w-full h-full bg-black">
+                                <div className="flex justify-center">
+                                    
+                                    <h2 className="text-white text-2xl">Loading...</h2>
+                                    <FontAwesomeIcon icon={faSpinner} size='lg' className="text-white animate-spin mx-4 mt-2"  />
                                 </div>
-                                <div className="flex flex-col items-start py-2">
-                                    <label className="text-white text-sm" htmlFor="name">Full Name</label>
-                                    <Field type="text" name="name" autocomplete="name" />
-                                </div>
-                                <button className="bg-lblue p-3 text-white font-serif rounded hover:bg-blue" type="submit" disabled={isSubmitting}>
-                                    Submit
-                                </button>
-                            </Form>
-                        )}
-                    </Formik>
+                            </div>
+                            : null
+                    }
+                    <iframe src="https://dtc-build.herokuapp.com/index.php" frameBorder="0" className="w-full my-5" height={window.innerHeight + "px"}
+                        onLoad={() => {
+                            setIsLoading(false)
+                            console.log("Loaded !")
+                        }} />
                 </div>
             </div>
         </main>
--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 const Contact = () => {
-    return(
-        <main>
+  return (
+    <main>
       <h1 className="bg-sky-500 text-stone-50">
         Contact Us
       </h1>
-      
+
     </main>
-    )
+  )
 }
 
 export default Contact
